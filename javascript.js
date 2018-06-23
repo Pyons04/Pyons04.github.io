f07d0b5c64575e189b0c32e06b2f1c5e9ad52ce2
COMPUTERNAME\user <broad.basic15@gmail.com>
2018-06-23 21:55:15 +1000

    add some screenshots

--- javascript.js
+++ javascript.js
@@ -33,6 +33,30 @@ var action = document.getElementById('readmore');
 action.addEventListener('click', pulldown, false);
 //portfoliosのpull downのdescription終
 
+//portfolios3のpull downのdescription始
+function pulldown3(){
+          console.log('clicked!');
+          var save   = document.getElementById('portfolio3').innerHTML;
+          document.getElementById('portfolio3').innerHTML = document.getElementById('portfolio3_active').innerHTML;
+      }
+
+var action = document.getElementById('readmore3');
+
+action.addEventListener('click', pulldown3, false);
+//portfolios3のpull downのdescription終
+
+//portfolios4のpull downのdescription始
+function pulldown4(){
+          console.log('clicked!');
+          var save   = document.getElementById('portfolio4').innerHTML;
+          document.getElementById('portfolio4').innerHTML = document.getElementById('portfolio4_active').innerHTML;
+      }
+
+var action = document.getElementById('readmore4');
+
+action.addEventListener('click', pulldown4, false);
+//portfolios4のpull downのdescription終
+
 //言語のスキルレベルがアニメーションで動く仕組み始
 function filling(){
           console.log('ruby clicked!');
